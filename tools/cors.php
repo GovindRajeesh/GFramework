52f88da9e4ca468d80e509b1f749ad45ea3a4f2e
GovindRajeesh <69973781+GovindRajeesh@users.noreply.github.com>
2021-03-25 20:55:15 +0400

    Update cors.php

--- tools/cors.php
+++ tools/cors.php
@@ -1,14 +1,13 @@
 <?php 
-// cors disabled for a time
 function cors(){
-//     $origins=array('3dd9fd352a37.ngrok.io');
+ $origins=array('3dd9fd352a37.ngrok.io');
 
-// if(in_array("https://{$_SERVER['HTTP_HOST']}",$origins)){
+if(in_array("https://{$_SERVER['HTTP_HOST']}",$origins)){
 
-//     header("Access-Control-Allow-Origin:https://{$_SERVER['HTTP_HOST']}");
+header("Access-Control-Allow-Origin:https://{$_SERVER['HTTP_HOST']}");
 
-// }else{
-//     header("Access-Control-Allow-Origin:{$origins[0]}");
-// }
+}else{
+ header("Access-Control-Allow-Origin:{$origins[0]}");
+ }
 }
 ?>
